6a8f9b673913d2ed8e997909a2e99fc00eb24179
Rushani Shawuti <ruhsane924@gmail.com>
2019-05-09 11:31:49 -0700

    Created Item entity and attributes

--- MOB_2.1_Tutorial_Starter_App/loaner/model/Loanee.swift
+++ MOB_2.1_Tutorial_Starter_App/loaner/model/Loanee.swift
@@ -8,20 +8,41 @@
 
 import UIKit
 
-class Loanee: NSObject {
+class Loanee: NSObject, NSCoding {
 
-        var name: String
-        var contactNumber: String?
+    var name: String
+    var contactNumber: String?
+    
+    // NSCoding protocol requires having Keys for our variables
+    enum Keys: String {
+        case name = "name"
+        case contactNumber = "contactNumber"
+    }
+    
+    init(name: String, contactNumber: String?) {
         
-        init(name: String, contactNumber: String?) {
-            
-            /** For Future Feature: Ability to access Contacts app:
-             init(name: String, profileImage: UIImage, contactNumber: String?) {
-             
-             self.profileImage = profileImage
-             **/
-            
-            self.name = name
-            self.contactNumber = contactNumber
-        }
+        /** For Future Feature: Ability to access Contacts app:
+         init(name: String, profileImage: UIImage, contactNumber: String?) {
+         
+         self.profileImage = profileImage
+         **/
+        
+        self.name = name
+        self.contactNumber = contactNumber
+    }
+    
+    // NSCoding protocol requires an encode method toencode our variables based on the Key
+    func encode(with aCoder: NSCoder) {
+        aCoder.encode(name, forKey: "name")
+        aCoder.encode(contactNumber, forKey: "contactNumber")
+    }
+    
+    // NSCoding protocol requires an init method which acts asa decoder for our variables based on the Key
+    required init?(coder aDecoder: NSCoder) {
+        name = aDecoder.decodeObject(forKey: "name") as! String
+        contactNumber = aDecoder.decodeObject(forKey: "contactNumber") as? String
+        super.init()
+    }
+    
+
 }
